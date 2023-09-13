aff39ea5253b6053cba73d9d4a7b9d869cc34583
Perrshan <xaveperry@gmail.com>
2023-09-13 10:07:42 -0600

    Added my name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        //String interpolation
+        int myInt = 5;
+        Console.WriteLine("my int equals" + myInt);
+        Console.WriteLine($"my int equals {myInt}");
+
+        //ReadLine
+        Console.Write("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"name = {name}");
+
+        //Conditionals
+        if (name == "Xave")
+        {
+            Console.WriteLine("Hey that's me");
+        }
+        else
+        {
+            Console.WriteLine($"Hi there {name}");
+        }
+
     }
 }
