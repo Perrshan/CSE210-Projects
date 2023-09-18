c1aba42844f9aada680019f5ae5e2fe3004a3e16
Perrshan <xaveperry@gmail.com>
2023-09-18 09:56:48 -0600

    Took notes in class

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,67 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+
+        //Prep 3
+        
+
+        Console.Write("Give me a random number: ");
+        String randomNumberString = Console.ReadLine();
+        int randomNumber = int.Parse(randomNumberString);
+
+        Console.Write("Guess the number: ");
+        string guessNumberString = Console.ReadLine();
+        int guessNumber = int.Parse(guessNumberString);
+
+        if (guessNumber > randomNumber)
+        {
+            Console.WriteLine("You guessed too high!");
+        }
+        else if (guessNumber < randomNumber)
+        {
+            Console.WriteLine("You guess too low!");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+
+        //While loop
+        var count = 0;
+        while (count < 5)
+        {
+            Console.WriteLine("Still going");
+            if (count > 5)
+            {
+                break;
+            }
+            ++count;
+        }
+
+        //Do while loop
+        count = 0;
+        do
+        {
+            Console.WriteLine("hit");
+            count++;
+            Console.WriteLine($"count = {count}");
+        }
+        while(count < 5);
+
+        //For loop
+        for (var i =0; i<5; ++i)
+        {
+            Console.WriteLine($"i = {i}");
+        }
+
+        //Foreach loop
+        var myInts = new int[]{1,2,3,4,5};
+        for(var i = 0; i < myInts.Length; ++i)
+        {
+            Console.WriteLine($"i = {myInts[i]}");
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        //Primitive types
+        //int i;
+        string s;
+        char c;
+        float f;
+        double d;
+        byte b;
+
+        //Lists = new keyword
+        //int[] array
+        List<int> myInts = new List<int>();
+        myInts.Add(2);
+        myInts.Add(55);
+        myInts.Remove(1);
+        myInts.Insert(0, 34);
+
+        List<string> myStrings = new List<string>();
+        myStrings.Add("Hello");
+
+        //Iterate over items
+        foreach (var i in myInts)
+        {
+            Console.WriteLine($"My int = {i}");
+        }
+
     }
 }
